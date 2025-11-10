5b3f8928e4fb795982ba2c67f55489d2cdbcf049
Itule Godstime <itulegodstime3546@gmail.com>
2025-11-10 15:59:30 +0100

    updated a default location

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react';
 import ErrorPage from './ErrorPage';
 import WeatherInfoPage from './WeatherInfoPage';
-import searchicon from "/src/assets/images/icon-search.svg"
+import searchicon from "/src/assets/images/icon-search.svg";
 import { imperialContext } from '../context/imperialContext';
 
 const MainContent = () => {
@@ -12,13 +12,21 @@ const MainContent = () => {
     const [citySuggestions, setCitySuggestions] = useState([]);
     let suggestionDelay = 500;
     const value = useContext(imperialContext);
-    const cityEndpoint = "https://geocoding-api.open-meteo.com/v1/search?name="
+    const cityEndpoint = "https://geocoding-api.open-meteo.com/v1/search?name=";
+
+   
+    const defaultCity = {
+        name: "Lagos",
+        country: "Nigeria",
+        latitude: 40.7128,
+        longitude: -74.0060
+    };
 
     async function handleInputChange(e) {
-        let value = e.target.value;
-        setSearchInput(value);
+        let valueInput = e.target.value;
+        setSearchInput(valueInput);
 
-        if (value.citySuggestions < 2) {
+        if (valueInput.length < 2) {
             setCitySuggestions([]);
             return;
         }
@@ -26,18 +34,18 @@ const MainContent = () => {
         clearTimeout(suggestionDelay);
         suggestionDelay = setTimeout(async () => {
             try {
-                const cityResponse = await fetch(cityEndpoint + value);
+                const cityResponse = await fetch(cityEndpoint + valueInput);
                 const cityInfo = await cityResponse.json();
 
                 if (cityInfo.results) {
-                    setCitySuggestions(cityInfo.results.slice(0, 5))
+                    setCitySuggestions(cityInfo.results.slice(0, 5));
                 } else {
                     setCitySuggestions([]);
                 }
             } catch (error) {
                 console.error(error);
             }
-        })
+        }, 500);
     }
 
     async function fetchWeatherData(lat, lon, city, country) {
@@ -45,109 +53,126 @@ const MainContent = () => {
         const precipUnit = value.imperial ? "inch" : "mm";
         const tempUnit = value.imperial ? "fahrenheit" : "celsius";
 
-        const weatherResponse = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}` +
-            `&current=temperature_2m,apparent_temperature,relative_humidity_2m,precipitation,wind_speed_10m,weathercode` +
-            `&hourly=temperature_2m,apparent_temperature,weathercode` +
-            `&daily=weather_code,temperature_2m_max,apparent_temperature_max` +
-            `&temperature_unit=${tempUnit}` +
-            `&wind_speed_unit=${windUnit}` +
-            `&precipitation_unit=${precipUnit}` +
-            `&timezone=auto`
-        );
-        const weatherInfo = await weatherResponse.json();
-        const current = weatherInfo.current;
-        const currentUnit = weatherInfo.current_units;
-        const daily = weatherInfo.daily;
-        const hourly = weatherInfo.hourly;
-
-        setWeatherData({
-            cityName: city,
-            cityCountry: country,
-            temperature: current.temperature_2m,
-            feelsLike: current.apparent_temperature,
-            humidity: current.relative_humidity_2m,
-            windSpeed: current.wind_speed_10m,
-            precipitation: current.precipitation,
-            windUnit: currentUnit.wind_speed_10m,
-            precipUnit: currentUnit.precipitation,
-            tempUnit: currentUnit.temperature_2m,
-            weathercode: current.weathercode,
-            dailyday: daily.time,
-            dailytemp: daily.apparent_temperature_max,
-            dailyfeelslike: daily.apparent_temperature_max,
-            dailyweathercode: daily.weather_code,
-            hourlyweathercode: hourly.weathercode,
-            hourlyTime: hourly.time,
-            hourlyTemp: hourly.temperature_2m,
-        });
-    }
-
-    async function handleSearch() {
-        setLoading(true);
-        setApiError(false);
         try {
-            const cityResponse = await fetch(cityEndpoint + searchInput);
-            const cityInfo = await cityResponse.json();
-            if (!cityInfo.results || cityInfo.results.length === 0) {
-                throw new Error("City not found");
-            }
-            const lat = cityInfo.results[0].latitude;
-            const lon = cityInfo.results[0].longitude;
-            const cityName = cityInfo.results[0].name;
-            const cityCountry = cityInfo.results[0].country;
-            await fetchWeatherData(lat, lon, cityName, cityCountry);
+            setLoading(true);
+            const weatherResponse = await fetch(
+                `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}` +
+                `&current=temperature_2m,apparent_temperature,relative_humidity_2m,precipitation,wind_speed_10m,weathercode` +
+                `&hourly=temperature_2m,apparent_temperature,weathercode` +
+                `&daily=weather_code,temperature_2m_max,apparent_temperature_max` +
+                `&temperature_unit=${tempUnit}` +
+                `&wind_speed_unit=${windUnit}` +
+                `&precipitation_unit=${precipUnit}` +
+                `&timezone=auto`
+            );
+            const weatherInfo = await weatherResponse.json();
+            const current = weatherInfo.current;
+            const currentUnit = weatherInfo.current_units;
+            const daily = weatherInfo.daily;
+            const hourly = weatherInfo.hourly;
+
+            setWeatherData({
+                cityName: city,
+                cityCountry: country,
+                temperature: current.temperature_2m,
+                feelsLike: current.apparent_temperature,
+                humidity: current.relative_humidity_2m,
+                windSpeed: current.wind_speed_10m,
+                precipitation: current.precipitation,
+                windUnit: currentUnit.wind_speed_10m,
+                precipUnit: currentUnit.precipitation,
+                tempUnit: currentUnit.temperature_2m,
+                weathercode: current.weathercode,
+                dailyday: daily.time,
+                dailytemp: daily.apparent_temperature_max,
+                dailyfeelslike: daily.apparent_temperature_max,
+                dailyweathercode: daily.weather_code,
+                hourlyweathercode: hourly.weathercode,
+                hourlyTime: hourly.time,
+                hourlyTemp: hourly.temperature_2m,
+            });
         } catch (error) {
-            console.error("Error fetching data:", error);
+            console.error("Error fetching weather:", error);
             setApiError(true);
         } finally {
             setLoading(false);
         }
     }
+
+    async function handleSearch() {
+        setApiError(false);
+        try {
+            if (!searchInput) return;
+
+            const cityResponse = await fetch(cityEndpoint + searchInput);
+            const cityInfo = await cityResponse.json();
+
+            if (!cityInfo.results || cityInfo.results.length === 0) {
+                throw new Error("City not found");
+            }
+
+            const city = cityInfo.results[0];
+            await fetchWeatherData(city.latitude, city.longitude, city.name, city.country);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            setApiError(true);
+        }
+    }
+
+   
     useEffect(() => {
-        if (!searchInput) return;
-        handleSearch();
-    }, [value.imperial])
+        fetchWeatherData(defaultCity.latitude, defaultCity.longitude, defaultCity.name, defaultCity.country);
+    }, [value.imperial]);
 
     return (
         <>
-            {
-                apiError ? <ErrorPage /> : <div className='mx-4 my-5 flex flex-col justify-center items-center'>
+            {apiError ? (
+                <ErrorPage />
+            ) : (
+                <div className='mx-4 my-5 flex flex-col justify-center items-center'>
                     <h1 className='bricolage text-5xl mt-3'>How's the sky looking today?</h1>
                     <div className='mt-10 mb-5 flex flex-col md:flex-row items-center gap-3 w-full md:w-min'>
                         <div className='flex relative items-center gap-3 bg-transparent-bg focus-within:ring ring-white ring-offset-[#02012B] ring-offset-2 py-2 px-4 rounded-md w-full md:w-100'>
                             <img className='w-4' src={searchicon} alt="search icon" />
-                            <input className='text-gray-300 outline-0 border-0 placeholder:text-gray-300' type="text" onChange={handleInputChange} placeholder='Search for a place...' />
-                            {
-                                citySuggestions.length > 1 && <div className='absolute top-12 z-10 right-0 bg-transparent-bg rounded-md w-full dm-sans-light'>
-                                    <div className=' p-2 flex flex-col gap-2'>
-                                        {
-                                            citySuggestions.map((city, index) => (
-                                                <p
-                                                    key={index}
-                                                    className='bg-transparent-bg-hover flex justify-between items-center p-2 rounded-md text-sm'
-                                                    onClick={()=>{
-                                                        setSearchInput(city.name);
-                                                        setCitySuggestions([]);
-                                                        fetchWeatherData(city.latitude, city.longitude, city.name, city.country)
-                                                    }}>
-                                                    {city.name}, {city.country}
-                                                </p>
-                                            ))
-                                        }
+                            <input
+                                className='text-gray-300 outline-0 border-0 placeholder:text-gray-300'
+                                type="text"
+                                onChange={handleInputChange}
+                                placeholder='Search for a place...'
+                                value={searchInput}
+                            />
+                            {citySuggestions.length > 1 && (
+                                <div className='absolute top-12 z-10 right-0 bg-transparent-bg rounded-md w-full dm-sans-light'>
+                                    <div className='p-2 flex flex-col gap-2'>
+                                        {citySuggestions.map((city, index) => (
+                                            <p
+                                                key={index}
+                                                className='bg-transparent-bg-hover flex justify-between items-center p-2 rounded-md text-sm'
+                                                onClick={() => {
+                                                    setSearchInput(city.name);
+                                                    setCitySuggestions([]);
+                                                    fetchWeatherData(city.latitude, city.longitude, city.name, city.country);
+                                                }}
+                                            >
+                                                {city.name}, {city.country}
+                                            </p>
+                                        ))}
                                     </div>
                                 </div>
-                            }
+                            )}
                         </div>
-                        <button className='bg-lightblue w-full md:w-24 py-2 px-4 rounded-md text-white focus:ring ring-lightblue ring-offset-[#02012B] ring-offset-2' onClick={handleSearch}>
+                        <button
+                            className='bg-lightblue w-full md:w-24 py-2 px-4 rounded-md text-white focus:ring ring-lightblue ring-offset-[#02012B] ring-offset-2'
+                            onClick={handleSearch}
+                        >
                             Search
                         </button>
                     </div>
                     {weatherData && <WeatherInfoPage loading={loading} weatherData={weatherData} />}
                 </div>
-            }
+            )}
         </>
-    )
-}
+    );
+};
 
-export default MainContent
+export default MainContent;
